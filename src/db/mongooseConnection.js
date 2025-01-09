ff9cae815480c1b4b9db493a179cb0ac5b49dc55
Babita00 <babitaasth07@gmail.com>
2025-01-09 20:55:44 +0545

    refactor mongoose connection utility to improve naming and export function

--- src/db/mongooseConnection.js
+++ src/db/mongooseConnection.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants";
 
-const connectDB=async()=>{
-    try {
-
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log(`MongoDB connected !! Host:${connectionInstance.connection.host}`);
-        
-        
-    } catch (error) {
-        console.log("MongoDB connection ERROR:",error);
-        process.exit(1)
-        
-        
-    }
-}
+const mongooseConnection = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `MongoDB connected !! Host:${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection ERROR:", error);
+    process.exit(1);
+  }
+};
+export { mongooseConnection };
