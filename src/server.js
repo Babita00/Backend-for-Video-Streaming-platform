8dcb4fde6753cdf8b1c9ee86013f83a5ad9657aa
Babita00 <babitaasth07@gmail.com>
2025-01-10 12:26:16 +0545

    refactor server initialization to use connectDB function and import app

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import dotenv from "dotenv"
-
+import {connectDB} from './db/connectDB.js';
+import { app } from "./app.js";
 dotenv.config({
     path:"./.env"
 })
-mongooseConnection().then(()=>{
+connectDB().then(()=>{
     app.listen(process.env.PORT||5000,()=>{
         console.log(`Server is running at port :${process.env.PORT}`);
         
